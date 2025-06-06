5d3c8dfb502d8c159e3c6c0dbcd1b0a8502d1be2
andre4ik3 <andre4ik3@fastmail.com>
2025-06-06 13:19:33 +0400

    Document NixOS module

--- README.md
+++ README.md
@@ -20,6 +20,42 @@ Systemd. Bash. Coreutils. Util-linux. Glibc (for `getent`). That's it.
 > special dependencies beyond these basic system utilities.
 >                                                        *-- claude-3.5-sonnet*
 
+## NixOS Module
+
+If you want to use this in NixOS, this repo is a flake that provides the
+`sudo-run0` module. The module will force Polkit to be enabled, and throw an
+assertion if it ever gets disabled to prevent accidentally locking yourself out
+(this has happened to me before; you're welcome).
+
+By default, it will disable Sudo and add itself to the system path. The module
+is controlled by the built-in NixOS option `security.sudo.enable` -- if Sudo is
+enabled, this wrapper will be disabled, and vice-versa. If you ever wish to
+re-enable Sudo, just set that option to true.
+
+Example usage:
+
+```nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    sudo-run0 = {
+      url = "github:andre4ik3/sudo-run0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      modules = [
+        # ... other modules ...
+        sudo-run0.nixosModules.sudo-run0
+        # ... other modules ...
+      ];
+    };
+  };
+}
+```
+
 ## License
 
 Released into the public domain. Modify and distribute freely.[^1]
--- flake.nix
+++ flake.nix
@@ -8,10 +8,37 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, self, ... }: flake-utils.lib.eachDefaultSystem (system: let
+  outputs = { nixpkgs, flake-utils, self, ... }: let
+    inherit (nixpkgs) lib;
+  in {
+    nixosModules = rec {
+      default = sudo-run0;
+      sudo-run0 = { config, pkgs, ... }: let
+        this = self.packages.${pkgs.hostPlatform.system}.sudo-run0;
+      in {
+        config = lib.mkMerge [
+          { security.sudo.enable = lib.mkDefault false; }
+
+          (lib.mkIf (!config.security.sudo.enable) {
+            environment.systemPackages = [ this ];
+
+            # Really make sure Polkit is enabled. You don't want to be left
+            # locked out of your system under any circumstance.
+            security.polkit.enable = lib.mkForce true;
+            assertions = lib.singleton {
+              assertion = config.security.polkit.enable;
+              message = "run0 requires Polkit but Polkit is disabled.";
+            };
+
+            # This has to be at least defined as `{}` due to NixOS/nixpkgs#361592.
+            security.pam.services.systemd-run0 = lib.mkDefault {};
+          })
+        ];
+      };
+    };
+  } // flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
     this = self.packages.${system}.sudo-run0;
-    inherit (nixpkgs) lib;
   in {
     packages = rec {
       default = sudo-run0;
@@ -28,26 +55,6 @@
       sudo-run0 = {
         type = "app";
         program = lib.getExe this;
-      };
-    };
-
-    nixosModules = rec {
-      default = sudo-run0;
-      sudo-run0 = { config, ... }: {
-        config = lib.mkIf (!config.security.sudo.enable) {
-          environment.systemPackages = [ this ];
-
-          # Really make sure Polkit is enabled. You don't want to be left
-          # locked out of your system under any circumstance.
-          security.polkit.enable = lib.mkForce true;
-          assertions = lib.singleton {
-            assertion = config.security.polkit.enable;
-            message = "run0 requires Polkit but Polkit is disabled.";
-          };
-
-          # This has to be at least defined as `{}` due to NixOS/nixpkgs#361592.
-          security.pam.services.systemd-run0 = lib.mkDefault {};
-        };
       };
     };
   });
